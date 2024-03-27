2f2121429f8f98b80de79ad80272dc5c1e98aa98
Dominic Menzel (Manni) <mannimanfred180@googlemail.com>
2024-03-27 23:25:22 +0100

    Add product details to DummyDataPage component

--- app/dummydata/page.jsx
+++ app/dummydata/page.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const DummyDataPage = () => {
+  const [product, setProduct] = useState([]);
+  const [error, setError] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://dummyjson.com/products');
+        setProduct(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
+
   return (
     <div>
-      
+      <h1>Product Details</h1>
+      <p>Name: {product.title}</p>
+      <p>Price: {product.price}</p>
+    
     </div>
   )
 }
