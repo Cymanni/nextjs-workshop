81d4cc8e768cd4bfdf804f37f94d9ed4d40e555a
Berbel <mannimanfred180@googlemail.com>
2024-06-13 15:57:57 +0200

    chore: Refactor DummyDataPage component and fix error handling

--- app/dummydata/page.jsx
+++ app/dummydata/page.jsx
@@ -4,15 +4,16 @@ import axios from 'axios';
 
 const DummyDataPage = () => {
   const [product, setProduct] = useState([]);
-  const [error, setError] = useState([]);
+  const [error, setError] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://dummyjson.com/products');
         setProduct(response.data);
+        console.log(response.data);
       } catch (error) {
-        setError(error);
+        setError(error, "error");
       }
     };
 
@@ -22,14 +23,13 @@ const DummyDataPage = () => {
   
 
   return (
+    <>
     <div>
-      <h1>Product Details</h1>
-      <p>Name: {product.title}</p>
-      <p>Price: {product.price}</p>
-    
-    </div>
-  )
+    <p>Name: {product.title}</p>
+    <p>Price: {product.price}</p>
+  </div>
+  </>
+  );
 }
-
 export default DummyDataPage
 
